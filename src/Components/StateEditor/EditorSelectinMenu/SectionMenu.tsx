06c0610158ddcceb612f5ae68b0671eec982e0fb
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-11-09 02:10:21 +0100

    Edit menu tabs work fine

--- src/Components/StateEditor/EditorSelectinMenu/SectionMenu.tsx
+++ src/Components/StateEditor/EditorSelectinMenu/SectionMenu.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import { useThemesAPI } from "../../../Contexts/ThemeContext";
+import { useStyles } from "./styles";
+
+const BOARD = 'Board';
+const GENERAL = 'General';
+
+const NavigationSection = ({name, setActive, currentActiveSection}: any) => {
+    const { theme } = useThemesAPI();
+    const classes: {[key:string]: string} = useStyles(theme as any);
+    const isActive = name === currentActiveSection;
+    return (
+        <span 
+            className={`${classes.tab} ${isActive ? classes.frontTab: classes.backTab}`}
+            
+        >
+            <div className={`${classes.trapezoid} ${isActive ? classes.activeTab: classes.bluredTab}`} onClick={() => setActive(name)}>
+                {name}
+            </div>
+        </span>
+    )
+}
 
 export const StateEditorNavigation = ({playersColors}: any) => {
+    const { theme } = useThemesAPI();
+    const classes: {[key:string]: string} = useStyles(theme as any);
+    const sections = [GENERAL, ...playersColors, BOARD]
+    const [activeSection, setActiveSection] = useState(sections[0])
+    useEffect(() => console.log(activeSection), [activeSection])
+    
     return (
-        <>
-            {playersColors}
-        </>
+        <div className={classes.navigationWrapper}>
+            {sections.map(
+                (sectionName) => <NavigationSection 
+                                name = {sectionName}
+                                setActive = {(val: string) => setActiveSection(val)}
+                                currentActiveSection = {activeSection}
+                            />)
+            }
+        </div>
     )
 }
--- src/Components/StateEditor/General/GeneralStateEditor.tsx
+++ src/Components/StateEditor/General/GeneralStateEditor.tsx
@@ -1,3 +1,4 @@
 export const GeneralStateEditor = () => {
+    
     return <></>
 }
