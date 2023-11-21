ede3edf519d17f51104bbc2e964972ed8425c68b
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-11-21 02:28:00 +0100

    Inputs added, number field box bug corrected

--- src/Components/Interactors/Input/styles.js
+++ src/Components/Interactors/Input/styles.js
@@ -2,6 +2,9 @@ import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles((theme) => ({
     container: {
-
-    }
+        display: 'flex',
+        columnGap: '1rem',
+        margin: '1rem',
+        width: '100%',
+    },
 }));
--- src/Constants/constants.ts
+++ src/Constants/constants.ts
@@ -11,3 +11,22 @@ export const NR_TURNS_TO_WAIT = 'nrTurnsToWait';
 export const IS_GAME_LOST = 'isGameLost';
 
 export const PAWN_DIAMETER = 32;
+
+export const NOTES = {
+    '1000': 20,
+    '500' : 30,
+    '100' : 50,
+    '50'  : 50,
+    '20'  : 30,
+    '10'  : 40,
+    '5'   : 30,
+}  // NOT VERIFIED !!!!!
+
+export const MONEY_ALLTOGETHER = (() => {
+    const result = Object.entries(NOTES).reduce((acc: number, [key, value]: [string, number]) => {
+        acc = (acc + (parseInt(key) * value));
+        return acc
+    }, 0);
+    console.log('Money alltogether', result)
+    return result;
+})();
