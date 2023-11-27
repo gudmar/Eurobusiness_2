ba52129ec38d2e70e2def7c96a3fca25d6f86646
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-11-27 02:39:57 +0100

    Some content of SelectFrom... is displayed on the screen

--- src/Components/Interactors/SelectFromList/SelectFromList.tsx
+++ src/Components/Interactors/SelectFromList/SelectFromList.tsx
@@ -1,39 +1,63 @@
-import { useEffect } from "react";
+import { useRef } from "react";
 import { useThemesAPI } from "../../../Contexts/ThemeContext";
 import { useStyles } from "./styles";
-import { iSelectFromListProps } from "./types";
+import { iItemProps, iSelectFromListProps } from "./types";
+import { useSelectFromLogic } from "./useSelectFromLogic";
 
-export const Checkbox = ({id, label, items, defaultValue, onClick}: iSelectFromListProps) => {
+const Item = ({value, selectedValue, select}:iItemProps) => {
     const { theme } = useThemesAPI();
     const classes: {[key:string]: string} = useStyles(theme as any);
+    const isSelected = selectedValue === value;
+    return (
+        <div 
+            className={`${classes.itemWrapper} ${isSelected ? classes.selected : classes.notSelected}`}
+            onClick={() => {select(value)}}
+        >
+            {value}
+        </div>
+    )
+}
+
+export const SelectFromList = ({id, label, items, defaultValue='', onClick}: iSelectFromListProps) => {
+    const { theme } = useThemesAPI();
+    const classes: {[key:string]: string} = useStyles(theme as any);
+    const textBoxReference = useRef<HTMLInputElement>(null);
     const {
         isSearchListExpanded,
-        // selected,
+        selected,
         valueInTextBox,
         selectItem,
         clearSelection,
         search,
-    } = useSelectFromLogic({textBoxReference, items, defaultSelection, onClick})
-
+        close,
+        open
+    } = useSelectFromLogic({textBoxReference, items, defaultSelection: defaultValue, onClick})
+    
     return (
         <div className={classes.container}>
-
-        <div className={classes.label}>
-            <label
-                htmlFor={id || label}
-            >
-                {label}
-            </label>
-        </div>
+            <div className={classes.label}>
+                <label
+                    htmlFor={id || label}
+                >
+                    {label}
+                </label>
+            </div>
         <div className={classes.input}>
             <input
                 type="text"
                 id={id || label}
-                
-                
+                ref={textBoxReference}
             />
+            <div className={classes.list}>
+                {items.map((value: string) => 
+                    <Item
+                        value={value}
+                        select={selectItem}
+                        selectedValue={selected}
+                    />
+                )}
+            </div>
         </div>
-
     </div>
     )
 }
