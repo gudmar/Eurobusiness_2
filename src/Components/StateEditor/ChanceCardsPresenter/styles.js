82cb9426f7b632285469ea6ba30a51af33d0f86e
Marek Gudalewicz <marek.gudalewicz@localhost>
2023-12-22 02:10:51 +0100

    Chance cards in admin panel are displayed as expected

--- src/Components/StateEditor/ChanceCardsPresenter/styles.js
+++ src/Components/StateEditor/ChanceCardsPresenter/styles.js
@@ -1,11 +1,43 @@
 import { createUseStyles } from "react-jss";
 
+const CARD_WIDTH = '15rem';
+const CARD_HEIGHT ='8rem';
+const HALF_WIDTH = '7rem';
+const PATH_LEFT = `
+polygon(
+    0% 0%,
+    0% 100%,
+    50% 100%,
+    50% 95%,
+    20% 75%,
+    7% 50%,
+    20% 25%,
+    50% 5%,
+    5% 0%
+)
+`;
+const PATH_RIGHT = `
+polygon(
+    100% 0%,
+    100% 100%,
+    50% 100%,
+    50% 95%,
+    80% 75%,
+    93% 50%,
+    80% 25%,
+    50% 5%,
+    5% 0%
+)
+`
 export const useStyles = createUseStyles((theme) => ({
     container: {
+        // positoin: 'relative',
         display: 'flex',
         overflow: 'auto',
         width: '100%', 
         flexWrap: 'wrap',
+        counterReset: 'Cards',
+        maxHeight: '100%',
     },
     center: {
         display: 'flex',
@@ -18,17 +50,42 @@ export const useStyles = createUseStyles((theme) => ({
     card: {
         color: '#333333',
         borderRadius: '0.5rem',
-        width: '15rem',
-        height: '8rem',
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT,
         padding: '0.5rem',
-        margin: '0.5rem'
+        margin: '0.5rem',
+        overflow: 'hidden',
+        counterIncrement: 'Card',
+        position: 'relative',
+        '&:before': {
+            position: 'absolute',
+            color: 'white',
+            fontWeight: 'bold',
+            content: 'counter(Card)',
+            left: '0.7rem'
+        }
+    },
+    'p': {
+        textAlign: 'center'
+    },
+    right: {
+        width: HALF_WIDTH,
+        height: '9rem',
+        // backgroundColor: '#99229988',
+        shapeOutside: PATH_RIGHT,
+        clipPath: PATH_RIGHT,
+    },
+    left: {
+        width: HALF_WIDTH,
+        height: '9rem',
+        // backgroundColor: '#99992288',
+        shapeOutside: PATH_LEFT,
+        clipPath: PATH_LEFT,
     },
     oval: {
         borderRadius: '50%',
         backgroundColor: 'white',
-        // width: '10rem',
-        shapeInside: 'ellipse(50% 50%)',
-        height: '7rem',
+        height: `${parseInt(CARD_HEIGHT.split('r')[0] - 1)}rem`,
     },
     redCard: {
         backgroundColor: '#ee3333',
--- src/Components/StateEditor/StateEditor/StateEditor.tsx
+++ src/Components/StateEditor/StateEditor/StateEditor.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from "react";
+import { useThemesAPI } from "../../../Contexts/ThemeContext";
 import { BLUE, GREEN, RED, YELLOW } from "../../../Data/const";
 import { usePlayersColors } from "../../../hooks/usePlayersColors"
 import { ChanceCardsPresenter } from "../ChanceCardsPresenter/ChanceCardsPresenter";
@@ -6,6 +7,7 @@ import { StateEditorNavigation } from "../EditorSelectinMenu/SectionMenu"
 import { EstatesStateEditor } from "../EstatesStateEditor/EstatesStateEditor";
 import { GeneralStateEditor } from "../General/GeneralStateEditor";
 import { PlayerStateEditor } from "../PlayerStateEditor/PlayerStateEditor";
+import { useStyles } from "./style";
 
 const BOARD = 'Board';
 const GENERAL = 'General';
@@ -34,18 +36,24 @@ const EDITORS: iEditors = {
 }
 
 export const PlayerEditor = () => {
+    const { theme } = useThemesAPI();
+    const classes: {[key:string]: string} = useStyles(theme as any);
     const playersColors = usePlayersColors();
     const sections = [GENERAL, ...playersColors, BOARD, RED_CARDS, BLUE_CARDS]
     const [activeSection, setActiveSection] = useState(sections[0]);
     const Editor = EDITORS[activeSection];
     return (
-        <>
-            <StateEditorNavigation
-                sections={sections}
-                activeSection={activeSection}
-                setActiveSection={setActiveSection}
-            />
-            <Editor section={activeSection}/>
-        </>
+        <div className={classes.container}>
+            <div className={classes.nav}>
+                <StateEditorNavigation
+                    sections={sections}
+                    activeSection={activeSection}
+                    setActiveSection={setActiveSection}
+                />
+            </div>
+            <div className={classes.edit}>
+                <Editor section={activeSection}/>
+            </div>
+        </div>
     )
 }
