2b2a6fd59b7608d11bda42587e563cb72a99e21a
Marek Gudalewicz <marek.gudalewicz@localhost>
2024-02-26 01:39:06 +0100

    Dodano obsługę języków w komponencie zapisującym stan gry, żeby przetestować sposób w jaki będzie to robione

---
+++ src/Components/SaveLoadGameWindow/languages.ts
@@ -0,0 +1,65 @@
+import { EN, PL } from "../../Contexts/CurrentLanguage/const";
+
+export const TEXT = {
+    title: {
+        [PL]: 'Zachowaj/wczytaj stan gry',
+        [EN]: 'Presist/load game state'
+    },
+    savedGamesListLabel: {
+        [PL]: 'Zachowane gry',
+        [EN]: 'Saved games'
+    },
+    name: {
+        [PL]: 'Nazwa',
+        [EN]: 'Name',
+    },
+    disabledNameTooltip: {
+        [PL]: 'Nie można ustawić jeżeli nazwa jest domyślna',
+        [EN]: 'Cannot set name if name is default',
+    },
+    search: {
+        [PL]: 'Wyszukaj nazwę',
+        [EN]: 'Search names',
+    },
+    proposeDefaultName: {
+        [PL]: 'Zaproponuj nazwę domyślną',
+        [EN]: 'Propose default name',
+    },
+    dropSelectionTooltip: {
+        [PL]: 'Należy wybrać grę',
+        [EN]: 'Game has to be selected'
+    },
+    dropSelection: {
+        [PL]: 'Odznacz',
+        [EN]: 'Drop selection'
+    },
+    save: {
+        [PL]: 'Zapisz',
+        [EN]: 'Save'
+    },
+    saveTooltip: {
+        [PL]: 'Nazwa gry nie może być pusta, nie można zapisać gry już istniejącej',
+        [EN]: 'Name cannot be empty, cannot save existing game'
+    },
+    loadTooltip: {
+        [PL]: 'Nazwa gry nie może być pusta i musi istnieć',
+        [EN]: 'Name cannot be empty, and must exist',
+    },
+    load: {
+        [PL]: 'Wczytaj',
+        [EN]: 'Load'
+    },
+    rename: {
+        [PL]: 'Zmień nazwę',
+        [EN]: 'Rename',
+    },
+    renameTooltip: {
+        [PL]: 'Naleźy wybrać grę i zmienić jej nazwę lub opis',
+        [EN]: 'Game has to be selected, and description or name has to be changed'
+    },
+    delete: {
+        [PL]: 'Usuń',
+        [EN]: 'Delete'
+    }
+
+}
